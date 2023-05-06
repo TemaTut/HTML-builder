6dcd96770afd0e92039f3ca0536956b3f114193e
Tematut <0009494@mail.ru>
2023-05-06 20:08:40 +0400

    feat: add 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,62 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  const files = path.join(__dirname, 'files');
+  const filesCopy = path.join(__dirname, 'files-copy');
+
+  fs.mkdir(filesCopy, (err) => {
+    if (err && err.code !== 'EEXIST') {
+      console.error(`Failed to create directory ${filesCopy}: ${err}`);
+      return;
+    }
+
+    fs.readdir(filesCopy, (err, destFiles) => {
+      if (err) {
+        console.error(`Failed to read directory ${filesCopy}: ${err}`);
+        return;
+      }
+
+      fs.readdir(files, (err, srcFiles) => {
+        if (err) {
+          console.error(`Failed to read directory ${files}: ${err}`);
+          return;
+        }
+
+        srcFiles.forEach((file) => {
+          const srcPath = path.join(files, file);
+          const destPath = path.join(filesCopy, file);
+
+          fs.readFile(srcPath, (err, data) => {
+            if (err) {
+              console.error(`Failed to read file ${srcPath}: ${err}`);
+              return;
+            }
+
+            fs.writeFile(destPath, data, (err) => {
+              if (err) {
+                console.error(`Failed to write file ${destPath}: ${err}`);
+                return;
+              }
+            });
+          });
+        });
+
+        destFiles.forEach((file) => {
+          if (!srcFiles.includes(file)) {
+            const destPath = path.join(filesCopy, file);
+
+            fs.unlink(destPath, (err) => {
+              if (err) {
+                console.error(`Failed to delete file ${destPath}: ${err}`);
+                return;
+              }
+            });
+          }
+        });
+      });
+    });
+  });
+}
+
+copyDir();
